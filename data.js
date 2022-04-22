60286e6ed05ad0c0e189267fde95c2ac09f0e1f1
a-yu19 <99371106+a-yu19@users.noreply.github.com>
2022-04-21 23:34:07 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,142 @@
+var jsonData2 = [
+	{
+	"q" : "Which of the following HTML tag is used to display the text with scrolling effect?",
+	"opt1" : "< marquee >",
+	"opt2" : "< scroll >",
+	"opt3" : "< div >",
+	"answer": "< marquee >"
+},
+{
+	"q" : "Which of the following HTML tag is the special formatting tag?",
+	"opt1" : "< p >",
+	"opt2" : "< pre >",
+	"opt3" : "< b >",
+	"answer": "< pre >"
+},
+{
+	"q" : "Which of the following is the correct way to send mail in HTML?",
+	"opt1" : "< a href = 'mailto: xy@y' >",
+	"opt2" : "< a href = 'xy@y' >",
+	"opt3" : "< mail xy@y</mail >",
+	"answer": "< a href = 'mailto: xy@y' >"
+},
+{
+	"q" : "Which of the following is the container for < tr >, < th >, and < td > ?",
+	"opt1" : "< data >",
+	"opt2" : "< group >",
+	"opt3" : "< table >",
+	"answer": "< table >"
+},
+{
+	"q" : "How to insert a background image in HTML?",
+	"opt1" : "< body background = 'img.png' >",
+	"opt2" : "< img background = 'img.png' >",
+	"opt3" : "< bg-image = 'img.png' >",
+	"answer": "< body background = 'img.png' >"
+},
+{
+	"q" : "What are the types of unordered or bulleted list in HTML?",
+	"opt1" : "disc, square, triangle",
+	"opt2" : "disc, circle, square",
+	"opt3" : "polygon, triangle, circle",
+	"answer": "disc, circle, square"
+},
+{
+	"q" : "Which of the following is the correct way to create a list using the lowercase letters?",
+	"opt1" : "< ol alpha = 'a'  >",
+	"opt2" : "< ol letter = 'a' >",
+	"opt3" : "< ol type = 'a' >",
+	"answer": "< ol type = 'a' >"
+},
+{
+	"q" : "Which of the following is the correct way to start an ordered list with the count of numeric value 4?",
+	"opt1" : "< ol type = '1'  start = '4' >",
+	"opt2" : "< ol type = '1' num = '4' >",
+	"opt3" : "< ol type = '1' begin = '4' >",
+	"answer": "< ol type = '1'  start = '4' >"
+},
+{
+	"q" : "Which of the following HTML attribute is used to define inline styles?",
+	"opt1" : "style",
+	"opt2" : "class",
+	"opt3" : "type",
+	"answer": "style"
+},
+{
+	"q" : "Which of the following is the paragraph tag in HTML?",
+	"opt1" : "< pre >",
+	"opt2" : "< p >",
+	"opt3" : "< b >",
+	"answer": "< p >"
+},
+{
+	"q" : "An HTML program is saved by using the ____ extension.",
+	"opt1" : ".html",
+	"opt2" : ".ht",
+	"opt3" : ".hml",
+	"answer": ".html"
+},
+{
+	"q" : "A program in HTML can be rendered and read by -",
+	"opt1" : "Interpreter",
+	"opt2" : "Server",
+	"opt3" : "Web browser",
+	"answer": "Web browser"
+},
+{
+	"q" : "The tags in HTML are -",
+	"opt1" : "in upper case",
+	"opt2" : "not case sensitive",
+	"opt3" : "case sensitive",
+	"answer": "not case sensitive"
+},
+{
+	"q" : "Which of the following is the root tag of the HTML document?",
+	"opt1" : "< html >",
+	"opt2" : "< title >",
+	"opt3" : "< head >",
+	"answer": "< html >"
+},
+{
+	"q" : "In HTML5, which of the following tag is used to initialize the document type?",
+	"opt1" : "< !DOCTYPE html >",
+	"opt2" : "< Doctype >",
+	"opt3" : "< Doctype HTML >",
+	"answer": "< !DOCTYPE html >"
+},
+{
+	"q" : "Which of the following tag is used to create a combo box (or drop-down box)?",
+	"opt1" : "< select >",
+	"opt2" : "< ul >",
+	"opt3" : "< list >",
+	"answer": "< select >"
+},
+{
+	"q" : "Which of the following are the attributes of the tag?",
+	"opt1" : "method",
+	"opt2" : "action",
+	"opt3" : "both of the above",
+	"answer": "both of the above"
+},
+{
+	"q" : "Which is the correct way to comment out something in HTML?",
+	"opt1" : "Using < !-- and -- >",
+	"opt2" : "Using < /-- and -/- >",
+	"opt3" : "Using < !-- and -! >",
+	"answer": "Using < !-- and -- >"
+},
+{
+	"q" : "Which HTML tag is used to display the power in expression, i.e., (x2 - y2)?",
+	"opt1" : "< sup >",
+	"opt2" : "< sub >",
+	"opt3" : "< p >",
+	"answer": "< sup >"
+},
+{
+	"q" : "Which of the following is the correct way to change the font face in HTML?",
+	"opt1" : "< font face = 'Calibri' > ……… < /font >",
+	"opt2" : "< font = 'Calibri' > ……… < /font >",
+	"opt3" : "< font name = 'Calibri' > ……… < /font >",
+	"answer": "< font face = 'Calibri' > ……… < /font >"
+},
+];
