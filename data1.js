ccfb83580a986c558eb9b883751d23f6a875dd27
a-yu19 <99371106+a-yu19@users.noreply.github.com>
2022-04-21 23:33:45 -0400

    Create data1.js

---
+++ data1.js
@@ -0,0 +1,144 @@
+var jsonData1 = [
+
+{
+	"q" : "Which of the following attribute is used to provide a unique name to an element?",
+	"opt1" : "class",
+	"opt2" : "id",
+	"opt3" : "type",
+	"answer": "id"
+},
+{
+	"q" : "HTML stands for -",
+	"opt1" : "HyperText Markup Language",
+	"opt2" : "HyperText and links Markup Language",
+	"opt3" : "HighText Machine Language",
+	"answer": "HyperText Markup Language"
+},
+{
+	"q" : "The correct sequence of HTML tags for starting a webpage is -",
+	"opt1" : "Head, Title, HTML, body",
+	"opt2" : "HTML, Body, Title, Head",
+	"opt3" : "HTML, Head, Title, Body",
+	"answer": "HTML, Head, Title, Body"
+},
+{
+	"q" : "Which of the following element is responsible for making the text bold in HTML?",
+	"opt1" : "< pre >",
+	"opt2" : "< b >",
+	"opt3" : "< br >",
+	"answer": "< b >"
+},
+{
+	"q" : "Which of the following tag is used for inserting the largest heading in HTML?",
+	"opt1" : "< h3 >",
+	"opt2" : "< h2 >",
+	"opt3" : "< h1 >",
+	"answer": "< h1 >"
+},
+{
+	"q" : "Which of the following tag is used to insert a line-break in HTML?",
+	"opt1" : "< br >",
+	"opt2" : "< a >",
+	"opt3" : "< pre >",
+	"answer": "< br >"
+},
+{
+	"q" : "How to create an unordered list (a list with the list items in bullets) in HTML?",
+	"opt1" : "< ol >",
+	"opt2" : "< ul >",
+	"opt3" : "< li >",
+	"answer": "< ul >"
+},
+{
+	"q" : "Which character is used to represent the closing of a tag in HTML?",
+	"opt1" : "/",
+	"opt2" : "!",
+	"opt3" : ".",
+	"answer": "/"
+},
+{
+	"q" : "How to create a hyperlink in HTML?",
+	"opt1" : "< a href = 'www.javatpoint.com' > javaTpoint.com < /a >",
+	"opt2" : "< a url = 'www.javatpoint.com' javaTpoint.com /a >",
+	"opt3" : "< a link = 'www.javatpoint.com' > javaTpoint.com < /a >",
+	"answer": "< a href = 'www.javatpoint.com' > javaTpoint.com < /a >"
+},
+{
+	"q" : "How to create an ordered list (a list with the list items in numbers) in HTML?",
+	"opt1" : "< ol >",
+	"opt2" : "< ul >",
+	"opt3" : "< li >",
+	"answer": "< ol >"
+},
+{
+	"q" : "Which of the following element is responsible for making the text italic in HTML?",
+	"opt1" : "< italic >",
+	"opt2" : "< i >",
+	"opt3" : "< it >",
+	"answer": "< i >"
+},
+{
+	"q" : "How to insert an image in HTML?",
+	"opt1" : "< img href = 'jtp.png' / >",
+	"opt2" : "< img url = 'jtp.png' / >",
+	"opt3" : "< img src = 'jtp.png' / >",
+	"answer": "< img src = 'jtp.png' / >"
+},
+{
+	"q" : "How to add a background color in HTML?",
+	"opt1" : "< marquee bg color: 'red' >",
+	"opt2" : "< marquee bgcolor = 'red' >",
+	"opt3" : "< marquee color = 'red' >",
+	"answer": "< marquee bgcolor = 'red' >"
+},
+{
+	"q" : "input is -",
+	"opt1" : "a format tag",
+	"opt2" : "an empty tag",
+	"opt3" : "none of the above",
+	"answer": "an empty tag"
+},
+{
+	"q" : "Which of the following tag is used to make the underlined text?",
+	"opt1" : "< u >",
+	"opt2" : "< i >",
+	"opt3" : "< ul >",
+	"answer": "< u >"
+},
+{
+	"q" : "How to create a checkbox in HTML?",
+	"opt1" : "< input type = 'checkbox' >",
+	"opt2" : "< input type = 'button' >",
+	"opt3" : "< input type = 'check' >",
+	"answer": "< input type = 'checkbox' >"
+},
+{
+	"q" : "Which of the following tag is used to define options in a drop-down selection list?",
+	"opt1" : "< select >",
+	"opt2" : "< list >",
+	"opt3" : "< option >",
+	"answer": "< option >"
+},
+{
+	"q" : "HTML tags are enclosed in-",
+	"opt1" : "< and >",
+	"opt2" : "{ and }",
+	"opt3" : "# and #",
+	"answer": "< and >"
+},
+{
+	"q" : "Which of the following tag is used to add rows in the table?",
+	"opt1" : "< tr > and < /tr >",
+	"opt2" : "< th > and < /th >",
+	"opt3" : "< td > and < /td >",
+	"answer": "< tr > and < /tr >"
+},
+{
+	"q" : "The < hr > tag in HTML is used for -",
+	"opt1" : "horizontal ruler",
+	"opt2" : "new line",
+	"opt3" : "new paragraph",
+	"answer": "horizontal ruler"
+},
+
+];
